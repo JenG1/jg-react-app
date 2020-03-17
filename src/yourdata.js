8241767ec4d02b03f28d616ff137d3fb49974db6
JGen1 <jengenes01@gmail.com>
2020-03-17 00:09:40 -0400

    Added additional projects to projects page

--- src/yourdata.js
+++ src/yourdata.js
@@ -1,28 +1,28 @@
 export default
     {
         headerTagline: [
-                        'Hello, My name is Jen.',
-                        'I am a Full Stack Developer ',
-                        'from Baltimore, MD.'
-    ],
+            'Hello, My name is Jen.',
+            'I am a Full Stack Developer ',
+            'from Baltimore, MD.'
+        ],
 
-        contactEmail:'jengenes410@gmail.com',
-        contactCell:'786-556-7579',
+        contactEmail: 'jengenes410@gmail.com',
+        contactCell: '786-556-7579',
 
         abouttext: "I am a Full Stack Web Developer with a background in Molecular Biology and life-long dedication to learning. Effective at combining creativity and problem solving to develop user-friendly applications. A team-player with strong attention to detail no matter the complexity of the project. Technical programming skills : HTML5, CSS3, Javascript, JQuery, Bootstrap, Firebase, Node Js, MySQL, MongoDB, Express, Handelbars.js & ReactJS ",
-        aboutImage:"",
+        aboutImage: "",
 
-       ShowAboutImage:true,
+        ShowAboutImage: true,
 
-       projects:[
-           {
-            id: 1,
-            title:'FoxHole Villa', //Project Title - Add Your Project Title Here
-             service:'Web App', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
-             imageSrc:"https://i.ibb.co/bgKJj8v/foxhole.png",
-             //Project URL - Add Your Project Url Here
-             url:'https://rabbit-the-fixer.herokuapp.com/'
+        projects: [
+            {
+                id: 1,
+                title: 'FoxHole Villa', //Project Title - Add Your Project Title Here
+                service: 'Web App', // Add Your Service Type Here
+                //Project Image - Add Your Project Image Here
+                imageSrc: "https://i.ibb.co/bgKJj8v/foxhole.png",
+                //Project URL - Add Your Project Url Here
+                url: 'https://rabbit-the-fixer.herokuapp.com/'
             },
             {
                 id: 2,
@@ -31,7 +31,7 @@ export default
                 imageSrc: "https://i.ibb.co/n1csZZ4/ms-icon-310x310.png",
                 url: 'https://off-the-menu.herokuapp.com/'
             },
-            { 
+            {
                 id: 3,
                 title: 'Employee Direct',
                 service: 'Web App',
@@ -44,11 +44,27 @@ export default
                 service: 'Web App',
                 imageSrc: "https://i.ibb.co/MRgqj27/workouttrakr.png",
                 url: 'https://workout-trakr.herokuapp.com/'
-           } 
+            },
+            {
+                id: 5,
+                title: 'Day Planner',
+                service: 'Planner',
+                imageSrc: "https://i.ibb.co/wMwqQRp/day-planner.png",
+                url: 'https://jeng1.github.io/Day-Planner/'
+            },
+            {
+                id: 5,
+                title: 'Weather Dash',
+                service: 'Planner',
+                imageSrc: "https://i.ibb.co/BTr9hB1/weather-dash.png",
+                url: 'https://jeng1.github.io/Weather-Dash/'
+            }
         ],
         social: [
-            {   name:'Github',
-                url:'https://github.com/JenG1'},
+            {
+                name: 'Github',
+                url: 'https://github.com/JenG1'
+            },
             {
                 name: 'LinkedIn',
                 url: 'https://www.linkedin.com/in/jennifer-genes'
